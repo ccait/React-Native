69e1f492d2f376d29f0a4cf2f4d1b2ceb2551ca9
ccait <360897160@qq.com>
2023-07-07 06:21:58 -0500

    first commit

---
+++ service.js
@@ -0,0 +1,13 @@
+// src/services/PlaybackService.js
+import TrackPlayer from 'react-native-track-player';
+import { Event } from 'react-native-track-player';
+
+export const PlaybackService = async function() {
+
+    TrackPlayer.addEventListener(Event.RemotePlay, () => TrackPlayer.play());
+
+    TrackPlayer.addEventListener(Event.RemotePause, () => TrackPlayer.pause());
+
+    // ...
+
+};
